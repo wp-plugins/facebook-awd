30f434b38bcf688081a09b964041b15139493424
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-11 13:51:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/trunk@396004 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/init.php
+++ inc/init.php
@@ -361,6 +361,6 @@ if($this->debug_active)
 	add_action('wp_footer',array(&$this,'debug_content'));
 
 //load text domain file
-load_plugin_textdomain($this->plugin_text_domain,'/wp-content/plugins/facebook-awd/langs','/facebook-awd/langs');
+load_plugin_textdomain($this->plugin_text_domain,false,dirname( plugin_basename( __FILE__ ) ) . '/languages/');
 
 ?>
