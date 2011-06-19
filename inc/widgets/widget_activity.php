e9215cbeb3fe8b8e4b0a73f0f3d52040e2a1d6a9
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-19 22:42:27 +0000

    Add French Translation
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/trunk@399036 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/widgets/widget_activity.php
+++ inc/widgets/widget_activity.php
@@ -37,7 +37,7 @@ class AWD_facebook_activity extends WP_Widget {
 		$activity['activity_use_content'] = (esc_attr($instance['activity_use_content']) == '' ? 0 : $instance['activity_use_content']);
 	
 	
-	    echo '<h2 style="text-align:center;color:#627AAD;border:1px solid #ccc; padding:5px;"><img style="vertical-align:middle;" src="'.$this->AWD_facebook->plugin_url_images.'facebook-mini.png" alt="facebook logo"/> '.__('Facebook Like Box',$this->AWD_facebook->plugin_text_domain).'</h2><br />';
+	    echo '<h2 style="text-align:center;color:#627AAD;border:1px solid #ccc; padding:5px;"><img style="vertical-align:middle;" src="'.$this->AWD_facebook->plugin_url_images.'facebook-mini.png" alt="facebook logo"/> '.__('Facebook Activity Box',$this->AWD_facebook->plugin_text_domain).'</h2><br />';
 
 	
 		//if paser xfbml enable change the message
