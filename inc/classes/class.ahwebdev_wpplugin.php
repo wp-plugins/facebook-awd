e5df2e661fec7ef307f35df074bc5350c4b7d51b
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-17 16:47:09 +0000

    Add release 0.9.7.4 fix php error with featured image and thumb support fix pop up not closing JS
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/tags/0.9.7.4@424945 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/classes/class.ahwebdev_wpplugin.php
@@ -0,0 +1,124 @@
+<?php
+/*
+* Class AHWEBDEV_wpplugin
+* All plugins can use this parent class
+* (C) 2011 AH WEB DEV
+* Hermann.alexandre@ahwebdev.fr
+* Last modification : 15/07/2011
+*/
+class AHWEBDEV_wpplugin{
+    /**
+    * public
+    * current_user
+    */
+    public $current_user = array();
+    /**
+    * Name of the file of hte plugin
+    */
+    public $file_name = "AWD_facebook.php";
+    /**
+    *return current user in object.
+    */
+	public function current_user(){
+	    global $current_user;
+		get_currentuserinfo();
+		$this->current_user = $current_user;
+	}
+    /**
+	* Return IP
+	*/
+	public function get_ip(){
+		if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		else
+			$ip = $_SERVER['REMOTE_ADDR'];        
+	}
+	/**
+	 * plugin version
+	 */
+	public function get_version(){
+		if (!function_exists('get_plugins'))
+	    	require_once(ABSPATH.'wp-admin/includes/plugin.php');
+	    $plugin_folder = get_plugins('/'.plugin_basename( dirname(dirname(dirname( __FILE__ )))));
+	    return $plugin_folder[$this->file_name]['Version'];
+	}
+	
+	/**
+	* Update Options from post
+	*/
+	public function update_options_from_post(){
+	    return $this->update_options($_POST);
+	}
+	/**
+	* Update Options from list of option
+	* $options array.
+	*/
+	public function update_options($options){
+	    if(is_array($options)){
+            foreach($options as $option=>$value){
+                update_option($option,$value);
+            }
+            return 1;
+        }else{
+            return 0;
+        }
+	}
+	/**
+	* Add / to end of a page
+	* add_filter('user_trailingslashit', array(&$this,'add_page_slash'),100,2);
+	*/
+    public function add_page_slash($string, $type){
+        global $wp_rewrite;
+        if(ereg(".html",$string) && $wp_rewrite->use_trailing_slashes)
+        	return untrailingslashit($string);
+        else
+        	return $string;
+    }
+	/**
+	* function to catch firest image in html
+	*/
+	public function catch_that_image($post_content="") {
+		global $post;
+		if($post_content=="" && is_object($post))
+			$post_content = $post->post_content;
+  		$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post_content, $matches);
+  		$first_img = $matches [1] [0];
+  		return $first_img;
+	}
+	/*
+	* Debug
+	* $var
+	* $detail 1 or 0
+	* $core bool
+	*/
+	public function Debug($var,$detail=0){
+			echo "<pre>";
+			if($detail != 0)
+				var_dump($var);
+			else
+				print_r($var);
+			echo "</pre>";
+	}
+	/**
+	* Admin Infos
+	*/
+	public function infos_content(){
+	    ?>
+	    <div style="text-align:center;">
+			<div class="header_AWD_facebook_wrap">
+				<h2 style="color:#627AAD;margin:0px;">
+					<img style="vertical-align:middle;" src="<?php echo $this->plugin_url_images; ?>facebook.png" alt="facebook logo" class="AWD_button_media" width="35" height="35"/>Facebook AWD
+					<sup style="color:#627AAD;font-size:0.6em;">v<?php echo $this->get_version(); ?></sup>
+				</h2>
+			</div>
+			<a href="http://wordpress.org/extend/plugins/facebook-awd/" target="_blank" class="uiButton uiButtonNormal"><?php _e('Rate this plugin',$this->plugin_text_domain); ?></a>
+			<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZQ2VL33YXHJLC" target="_blank" class="uiButton uiButtonNormal"><?php _e('Donate',$this->plugin_text_domain); ?></a>
+			<a href="http://www.ahwebdev.fr/contact/" target="_blank" class="uiButton uiButtonNormal">Contact</a><br /><br />
+			<?php echo do_shortcode('[AWD_likebutton url="http://www.ahwebdev.fr/plugins/facebook-awd.html" layout="standart" width="258" height="30" faces="0" xfbml="0"]'); ?><br />
+			<?php echo do_shortcode('[AWD_likebox url="http://www.ahwebdev.fr" colorscheme="light" stream="0" xfbml="0" header="0" width="257" height="180" faces="1"]'); ?>
+	    </div>
+	    <p><?php printf(__('Please if you find Bug, report it to help us. You can report bug %shere%s',$this->plugin_text_domain),'<a href="http://wordpress.org/tags/facebook-awd?forum_id=10#postform">','</a>'); ?></p>
+	    <?php
+	}
+}
+?>
---
+++ js/facebook_awd.js
@@ -0,0 +1,64 @@
+/*
+*
+* JS AWD FCBK
+* (C) 2011 AH WEB DEV
+* Hermann.alexandre@ahwebdev.fr
+*
+*/
+
+//Hide or show state
+function hide_state(elem,elem_to_hide){
+	if(jQuery(elem).attr('checked') == false){
+		jQuery(elem_to_hide).fadeOut();
+	}else{
+		jQuery(elem_to_hide).show();
+	}
+}
+//Jquery Init
+jQuery(document).ready( function(){
+	//accordion brefore tabs
+	var icons = {
+		header: "ui-icon-circle-arrow-e",
+		headerSelected: "ui-icon-circle-arrow-s"
+	};
+	jQuery("[id^='ogtags_']:not(div#ogtags_taxonomies > div.ui_ogtags_form)").accordion({
+		header: "h4",
+		autoHeight: false,
+		icons:icons,
+		collapsible: true,
+		active: false
+		//TODOnavigation: true
+	});
+	
+	//create the tabs in options pages admin
+	jQuery("#div_options_content_tabs").tabs({ 
+		fx: { opacity: 'toggle',duration:'fast'},
+		cookie: { expires: 30 }
+	});
+	//hide all on load
+	jQuery("h3.tabs_in_tab").next().hide();
+	
+	//on click toogle next elem
+	jQuery("h3.tabs_in_tab a").click(function(e){
+		jQuery(this).parent().next().slideToggle();
+		e.preventDefault();
+	});
+	
+	//for the shortcode in admin form		
+	jQuery(".awd_pre").addClass('ui-corner-all');
+	//get the focus element
+	var id_focused;
+	jQuery(":input").focus(function () {
+		id_focused = this.id;
+	});
+
+	//when click on the tag, set it in focus elem
+	jQuery(".awd_pre b").click(function(){	
+		var b = jQuery(this);
+		var value = jQuery("#"+id_focused).val();
+		jQuery("#"+id_focused).val(value + b.html());
+	});
+	
+	//hide fadelement
+	jQuery(".fadeOnload").delay(4000).fadeOut();
+});
