c91ad5b0896c98521dc38f7103225875b1b65ce8
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-18 19:42:57 +0000

    dev
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/trunk@439758 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/init.php
+++ inc/init.php
@@ -56,6 +56,9 @@ add_action('wp_loaded',array(&$this,'wp_init'));
 if($this->debug_active)
 	add_action('wp_footer',array(&$this,'debug_content'));
 
+
+do_action("AWD_facebook_save_settings");
+
 //Get options from bdd
 $AWD_options = $this->wpdb->get_results("SELECT option_name,option_value FROM ".$this->wpdb->options." WHERE option_name LIKE '%".$this->plugin_option_pref."%'",'OBJECT');
 foreach($AWD_options as $options=>$object){
@@ -64,7 +67,6 @@ foreach($AWD_options as $options=>$object){
 
 //load some plugin of FACEBOOK AWD if exists
 do_action("AWD_facebook_plugins_init");
-do_action("AWD_facebook_save_settings");
 
 //apply filter hook for all options
 $this->plugin_option = apply_filters('AWD_facebook_options', $this->plugin_option);
