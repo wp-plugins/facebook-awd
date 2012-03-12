34a006b802fe7fb4bd899f1602df35ec2d5c329f
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-12 06:28:05 +0000

    fix html ogp
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/tags/0.9.9.1@517792 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- AWD_facebook.php
+++ AWD_facebook.php
@@ -1654,8 +1654,8 @@ Class AWD_facebook
 	public function get_the_open_graph_tags($options=array())
 	{
 		if(!empty($options)){
+			$html = '';
 			foreach($options as $tag=>$tag_value){
-				$html = '';
 				//custom for video TYPE
 				$tag = str_replace(array(":mp4","_mp4",":html","_html",'_custom'),array(""),$tag);
 				$html .= '<meta property="'.$tag.'" content="'.stripcslashes($tag_value).'"/>'."\n";
