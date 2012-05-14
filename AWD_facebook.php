070ce1751c5abf0b1fef3904e9f12c63027cc4cd
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-14 15:24:26 +0000

    1.3 pre1
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/trunk@544145 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- AWD_facebook.php
+++ AWD_facebook.php
@@ -3,7 +3,7 @@
 Plugin Name: Facebook AWD All in One
 Plugin URI: http://www.ahwebdev.fr
 Description: This plugin integrates Facebook open graph, Plugins from facebook, and FB connect, with SDK JS AND SDK PHP Facebook
-Version: 1.2.1
+Version: 1.3
 Author: AHWEBDEV
 Author URI: http://www.ahwebdev.fr
 License: Copywrite AHWEBDEV
