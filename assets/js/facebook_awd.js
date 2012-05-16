fd4dc9043b72ec8ae3dc6fe2882d4e1f000d50b9
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-16 17:33:58 +0000

    pre new release 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/trunk@544986 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/facebook_awd.js
@@ -0,0 +1,63 @@
+/*
+*
+* JS AWD FCBK
+* (C) 2012 AH WEB DEV
+* Hermann.alexandre@ahwebdev.fr
+*
+*/
+
+var AWD_facebook = {
+	
+	FBEventHandler : function ()
+	{
+		if(awd_fcbk.FBEventHandler.callbacks){
+			jQuery.each(awd_fcbk.FBEventHandler.callbacks,function(index,value){
+				var AWD_actions_callback = window[this];
+				if(jQuery.isFunction(AWD_actions_callback))
+					AWD_actions_callback(this);
+			});
+		}
+	},
+	
+	callbackLogin : function(response,redirect_url)
+	{
+		
+		var redirect = '';
+		if(response.authResponse){
+			if(!redirect_url){
+				window.location.href = awd_fcbk.loginUrl;
+			}else{
+				redirect = "?redirect_to="+redirect_url;
+				window.location.href = awd_fcbk.loginUrl+redirect;
+			}
+		}
+	},
+	
+	connect :function(redirect_url)
+	{
+		FB.login(
+			function(response){
+				AWD_facebook.callbackLogin(response,redirect_url);
+			},
+			{ 
+				scope: awd_fcbk.scope
+			}
+		);
+		return false;
+	},
+	
+	logout : function(){
+		window.location.href = awd_fcbk.logoutUrl;
+	},
+	
+	isFbConnected : function(){
+		var connected = false;
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				connected = true;
+				AWD_facebook.access_token = response.authResponse.accessToken;
+			}
+		});
+		return connected;		
+	},
+};
