eda0f05da607784a1272bd85bbdfeca30e814d7b
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-11 00:02:59 +0000

    V1.4
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd/trunk@597231 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/facebook_awd.js
@@ -0,0 +1,71 @@
+/**
+ * 
+ * @author alexhermann
+ *
+ */
+var AWD_facebook = {
+	
+	FBEventHandler : function ()
+	{
+		if(awd_fcbk.FBEventHandler.callbacks){
+			jQuery.each(awd_fcbk.FBEventHandler.callbacks,function(index,value){
+				var AWD_actions_callback = window[this];
+				if(jQuery.isFunction(AWD_actions_callback))
+					AWD_actions_callback(this);
+			});
+		}
+	},
+	
+	callbackLogin : function(response,redirect_url)
+	{
+		
+		var redirect = '';
+		if(response.authResponse){
+			if(!redirect_url){
+				window.location.replace(awd_fcbk.loginUrl);
+			}else{
+				redirect = "?redirect_to="+redirect_url;
+				window.location.replace(awd_fcbk.loginUrl+redirect);
+			}
+		}
+	},
+	
+	connect :function(redirect_url)
+	{
+		FB.login(
+			function(response){
+				AWD_facebook.fbConnected = true;
+				AWD_facebook.callbackLogin(response,redirect_url);
+			},
+			{ 
+				scope: awd_fcbk.scope
+			}
+		);
+		return false;
+	},
+	
+	logout : function(){
+		window.location.href = awd_fcbk.logoutUrl;
+	},
+	
+	fbConnected : false,
+	
+	isFbConnected : function(){
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				AWD_facebook.fbConnected = true;
+				AWD_facebook.access_token = response.authResponse.accessToken;
+			}
+		});
+		return AWD_facebook.fbConnected;	
+	},
+};
+
+jQuery(document).ready(function($){
+	//add usefull tool for creating connect button by hand.
+	$('.AWD_facebook_connect_button').live('click',function(e){
+		e.preventDefault();
+		var redirect = $(this).data('redirect');
+		AWD_facebook.connect(redirect);
+	});
+});
